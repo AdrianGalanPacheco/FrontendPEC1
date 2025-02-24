b30a6a5ba0c918214844a810438cbc32305ff03e
adrian galan pacheco <agalanpa@uoc.edu>
2025-02-24 16:19:40 +0100

    Added loading and error messages and non negative numbers

--- PEC1_Ej2_2/script.js
+++ PEC1_Ej2_2/script.js
@@ -6,20 +6,50 @@ const amountEl_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
+const loadingErrorMessageEl = document.getElementById("loadingErrorMessage");
+
+// Show loading
+function showMessage(type, message) {
+  loadingErrorMessageEl.className = `loadingErrorMessage ${type}`;
+  const small = loadingErrorMessageEl.querySelector("small");
+  small.innerText = message;
+}
+
+// Hide message
+function hideMessage() {
+  loadingErrorMessageEl.className = "loadingErrorMessage";
+  const small = loadingErrorMessageEl.querySelector("small");
+  small.innerText = "";
+}
+
 // Fetch exchange rates and update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then((res) => res.json())
-    .then((data) => {
-      const rate = data.rates[currency_two];
+  if (amountEl_one.value >= 0) {
+    showMessage("loading", "Loading...");
 
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          showMessage("error", data.error);
+        }
+        const rate = data.rates[currency_two];
 
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+        rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+        amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+
+        hideMessage();
+      })
+      .catch((error) => {
+        showMessage("error", error.message);
+      });
+  } else {
+    showMessage("error", "The amount must be positive");
+  }
 }
 
 // Event listeners
